d5893481a0dcd88360ea9e437a282d1637e4e213
Mico75 <michel.cotton1959@icloud.com>
2023-02-24 16:03:08 +0100

    Initial Commit

---
+++ GitHub_Exercice/GitHub_ExerciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciceApp.swift
+//  GitHub_Exercice
+//
+//  Created by Michel COTTON on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
